651d81dd7f2ec31fe3a691d5a05e18b1706fef98
ADONYARIK <scityrchenko@gmail.com>
2025-11-20 21:21:20 +0100

    Adding project base

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Phaser from 'phaser';
+import BootScene from './scenes/BootScene';
+import MainMenuScene from './scenes/MainMenuScene';
+import GameScene from './scenes/GameScene';
+import UIScene from './scenes/UIScene';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1600,
+    height: 900,
+    backgroundColor: '0xba6aa7',
+    parent: 'game-container',
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: [BootScene, MainMenuScene, GameScene, UIScene]
+};
+
+new Phaser.Game(config);
---
+++ src/scenes/BootScene.js
@@ -0,0 +1,37 @@
+import Phaser from 'phaser';
+
+export default class BootScene extends Phaser.Scene {
+    constructor() {
+        super('BootScene');
+    }
+
+    preload() {
+        this.load.image('logo', './src/assets/logo.png');
+    }
+
+    create() {
+        const { width, height } = this.scale;
+        const logo = this.add.image(width / 2, height / 2, 'logo').setScale(0.8);
+
+        this.loadAssets();
+
+        this.load.on('complete', () => {
+            logo.destroy();
+
+            this.scene.start('MainMenuScene');
+        })
+
+        this.load.start();
+    }
+
+    loadAssets() {
+        this.load.atlas('gui', './src/assets/atlas/gui_spritesheet.png', './src/assets/atlas/gui_spritesheet.json');
+        this.load.atlas('sprites', './src/assets/atlas/spritesheet.png', './src/assets/atlas/spritesheet.json');
+
+        WebFont.load({
+            google: { families: ['Jacquard 12'] }
+        });
+
+        this.load.audio('theme', './src/assets/audio/music/cottagecore.mp3');
+    }
+}
