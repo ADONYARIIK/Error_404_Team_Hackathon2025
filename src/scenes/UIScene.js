8b8263bec629e66bb1d50f479176626d6dcc7e02
Tymofii Sova <timsova2004@gmail.com>
2025-11-22 08:03:09 +0100

    adding gameOverScene

--- src/scenes/UIScene.js
+++ src/scenes/UIScene.js
@@ -11,13 +11,7 @@ export default class UIScene extends Phaser.Scene {
 
     create() {
         this.add.image(0,0,'gui','healthBar.png').setOrigin(0).setScale(3);
-        const healthBar = this.add.rectangle(54,47,56,11,0x5F1818,)
-
-        const game = this.add.image(0, 0, 'gui', 'gameName.png').setOrigin(0).setScale(0.5).setInteractive({ useHandCursor: true });
-
-        game.on('pointerdown', () => {
-            healthBar.width -= 10;
-        })
+        this.add.rectangle(54,47,56,11,0x5F1818,)
 
         const initialScores = this.registry.get('scores') || 0;
         this.scoresText = this.add.text(980, -10, this.formatScores(initialScores), {
